711534988d72cfb3f0ecef3f638915559df22e26
jscheu <joshua.scheu@gmail.com>
2023-09-21 16:16:30 -0500

    finished 3.6

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ let persons = [
     }
 ]
 
+const generateId = () => {
+    let rand = Math.floor(
+        Math.random() * 10000
+    )
+
+    if(persons.find(p => p.id === rand)) {
+        rand = generateId()
+    }
+
+    return rand
+}
+
 app.get('/info', (request, response) => {
     const personsInfo = `Phonebook has info for ${persons.length} people`
     const timeStamp = new Date().toISOString()
@@ -39,6 +51,51 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if(person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+
+    if(!person.name) {
+        return response.status(400).json({
+            error: 'missing name'
+        })
+    } else if(!person.number) {
+        return response.status(400).json({
+            error: 'missing number'
+        })
+    }
+
+    if(persons.find(p =>
+        p.name.toLowerCase() === person.name.toLowerCase())) {
+        return response.status(400).json({
+            error: `phonebook already contains ${person.name}`
+        })
+    }
+
+    person.id = generateId()
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
