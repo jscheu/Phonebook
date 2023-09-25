b96fb5fb9672ec143352697736df0191d347863e
jscheu <joshua.scheu@gmail.com>
2023-09-25 14:06:58 -0500

    Deployed to fly.io and updated REST Client URLs

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (request, response) => {
     return JSON.stringify(request.body)
@@ -113,7 +115,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
